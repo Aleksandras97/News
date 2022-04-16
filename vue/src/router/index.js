3973a3396cea997372dc1ae187c5ac5380e943b5
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2022-04-16 17:01:19 +0300

    Added dashboard, default layout and connected it to router

---
+++ vue/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard',
+    component: DefaultLayout,
+    children: [
+      {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router;
---
+++ vue/src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from 'vuex'
+
+const store = createStore({
+  state: {
+    feed: {
+      data: {title: 'First feed'},
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {}
+})
+
+export default store;
