d7da6e1052d4407ca0e19f70632b79d41b5e97fc
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2022-04-17 23:47:40 +0300

    Added page component

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,7 +3,11 @@ import {createStore} from 'vuex'
 const store = createStore({
   state: {
     feed: {
-      data: {title: 'First feed'},
+      data: {
+        title: 'Feed title',
+        category: 'Feed Category',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quae quibusdam, maiores, tenetur non eaque, accusantium dolorum iste nobis atque officiis id! Sed, laborum? Nemo voluptatibus sint eum cupiditate aperiam!'
+      },
     }
   },
   getters: {},
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,24 +1,7 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">News</h1>
-    </div>
-  </header>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-      </div>
-      <!-- /End replace -->
-    </div>
+  <PageComponent title="News">Content</PageComponent>
 </template>
 
-<script>
-export default {
-  name: "Dashboard"
-}
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
 </script>
-
-<style>
-
-</style>
