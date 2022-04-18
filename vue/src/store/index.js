a0236f46353ccc9c8d6fca1077de51c86b7c12ca
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2022-04-18 21:17:01 +0300

    Added axios, base url and actions to get data from API

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,127 +1,49 @@
+import axios from 'axios'
 import {createStore} from 'vuex'
+import axiosClient from '../axios';
 
-const tmpFeeds = [
-  {
-    id: 100,
-    title: 'First feed title',
-    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-    category: 'First category',
-    created_at: '2022-12-20 10:00:00',
-    updated_at: '2022-12-20 10:00:00',
-    feedProdider: {
-      id: 12,
-      name: 'Prodider name 1'
-    }
-  },
-  {
-    id: 101,
-    title: '2 feed title',
-    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-    category: 'Second category',
-    created_at: '2022-12-20 10:00:00',
-    updated_at: '2022-12-20 10:00:00',
-    feedProdider: {
-      id: 12,
-      name: 'Prodider name 1'
-    }
-  },
-  {
-    id: 102,
-    title: '3 feed title',
-    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-    category: 'First category',
-    created_at: '2022-12-20 10:00:00',
-    updated_at: '2022-12-20 10:00:00',
-    feedProdider: {
-      id: 13,
-      name: 'Prodider name 2'
-    }
-  },
-  {
-    id: 103,
-    title: '4 feed title',
-    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-    category: 'Second category',
-    created_at: '2022-12-20 10:00:00',
-    updated_at: '2022-12-20 10:00:00',
-    feedProdider: {
-      id: 13,
-      name: 'Prodider name 2'
-    }
-  },
-]
-
-const tpmFeedProviders = [
-  {
-    id: 12,
-    name: 'FeedProvider 1',
-    feeds: [
-      {
-        id: 103,
-        title: '1 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      },
-      {
-        id: 103,
-        title: '2 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      },
-      {
-        id: 103,
-        title: '3 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      }
-    ]
-  },
-  {
-    id: 13,
-    name: 'FeedProvider 2',
-    feeds: [
-      {
-        id: 103,
-        title: '1 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      },
-      {
-        id: 103,
-        title: '2 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      },
-      {
-        id: 103,
-        title: '3 feed title',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim adipisci similique alias dicta aliquam, asperiores maiores earum itaque inventore nesciunt aliquid suscipit repellendus unde omnis fugiat, delectus cum dolores ipsum?',
-        category: 'Second category',
-        created_at: '2022-12-20 10:00:00',
-        updated_at: '2022-12-20 10:00:00',
-      }
-    ]
-  },
-]
 
 const store = createStore({
   state: {
-    feeds: [...tmpFeeds],
-    feedProviders: [...tpmFeedProviders]
+    feeds: {
+      loading: false,
+      data: []
+    },
+    feedProvider: {
+      loading: false,
+      data: []
+    },
   },
   getters: {},
-  actions: {},
-  mutations: {},
+  actions: {
+    getFeeds({commit}) {
+      commit('setFeedsLoading', true);
+      return axiosClient.get('/feeds').then((res) => {
+        commit('setFeedsLoading', false);
+        commit('setFeeds', res.data);
+        return res;
+      })
+    },
+    getFeedProvider({commit}, id) {
+      commit('setFeedsLoading', true);
+      return axiosClient.get(`/feedProvider/${id}`).then((res) => {
+        commit('setFeedsLoading', false);
+        commit('setFeedProvider', res.data);
+        return res;
+      })
+    }
+  },
+  mutations: {
+    setFeeds: (state, feeds) => {
+      state.feeds.data = feeds.data;
+    },
+    setFeedProvider: (state, feedProvider) => {
+      state.feedProvider.data = feedProvider.data;
+    },
+    setFeedsLoading: (state, loading) => {
+      state.feeds.loading = loading;
+    }
+  },
   modules: {}
 })
 
